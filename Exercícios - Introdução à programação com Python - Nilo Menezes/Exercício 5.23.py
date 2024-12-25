67b4a877539b47df5b3d2a1e3f52c7ca5b40ed1d
Marcel Cardoso <marcelmessiascardoso@gmail.com>
2024-12-25 17:47:32 -0300

    Update Exercício 5.23.py

--- Exercícios - Introdução à programação com Python - Nilo Menezes/Exercício 5.23.py
+++ Exercícios - Introdução à programação com Python - Nilo Menezes/Exercício 5.23.py
@@ -3,25 +3,26 @@
 while True:
     print("Digite um número e verifique se ele é primo: ")
     entrada = int(input(""))
-    limite = entrada
-    contador = 1
+    contador = 3
     verificação1 = entrada % 2
     status = False
-    while True:
-        if verificação1 == 0:
-            status = True
-            
-            print("%d não é um número primo" % (entrada))
-        else:
-            contador = 1
-            while contador >= entrada:
-                operação = entrada % contador
-                if operação == 0:
-                    status = True
-                    print("%d não é um número primo" % (entrada))
-                    break
-                contador += 1
-        break
-    if status == False:
+    if entrada == 2:
         print("%d é um número primo!" % (entrada))
-    
+    else:
+        while True:
+            if verificação1 == 0:
+                status = True
+                
+                print("%d não é um número primo" % (entrada))
+            else:
+                contador = 1
+                while contador >= entrada:
+                    operação = entrada % contador
+                    if operação == 0:
+                        status = True
+                        print("%d não é um número primo" % (entrada))
+                        break
+                    contador += 1
+            break
+        if status == False:
+            print("%d é um número primo!" % (entrada))
