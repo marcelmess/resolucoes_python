6b92f528b6145b2f0832ff2a21acf19f2eb9c3f7
Marcel Cardoso <marcelmessiascardoso@gmail.com>
2024-12-28 15:45:54 -0300

    Update Exercício 6.8.py

--- Exercícios - Introdução à programação com Python - Nilo Menezes/Exercício 6.8.py
+++ Exercícios - Introdução à programação com Python - Nilo Menezes/Exercício 6.8.py
@@ -1,4 +1,3 @@
-
 L=[15,7,27,39]
 p=int(input("Digite o valor a procurar:"))
 x=0
@@ -7,7 +6,4 @@ while x<len(L):
     if L[x]==p:
         print("%d achado na posição %d" % (p,x))
         break
-        x+=1
-    else:
-        print("Não há esse valor na lista")
-        break
+    x += 1
