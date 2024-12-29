07bacd39ebbe2009eff214c9c8f0157c81a0dc5a
Marcel Cardoso <marcelmessiascardoso@gmail.com>
2024-12-29 19:16:30 -0300

    Add files via upload

---
+++ Exercícios - Introdução à programação com Python - Nilo Menezes/Exercício 6.16.py
@@ -0,0 +1,19 @@
+lista = [5, 10, 36, 76, 93, 3, 24, 55]
+
+fim = len(lista)
+
+while fim > 1:
+    trocou = False
+    x = 0
+    while x<(fim-1):
+        if lista[x] < lista[x+1]:
+            trocou = True
+            temp = lista[x]
+            lista[x] = lista[x+1]
+            lista[x+1] = temp
+        x+=1
+    if not trocou:
+        break
+    fim -= 1
+for valor in lista:
+    print(valor)
