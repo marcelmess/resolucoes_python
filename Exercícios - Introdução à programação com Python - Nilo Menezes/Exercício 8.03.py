68bead98b2f4b699dfafa4124a1c27359bdcdbe2
Marcel Cardoso <marcelmessiascardoso@gmail.com>
2025-01-06 18:39:09 -0300

    Add files via upload

---
+++ Exercícios - Introdução à programação com Python - Nilo Menezes/Exercício 8.03.py
@@ -0,0 +1,4 @@
+def cálculo(a):
+    return (a*a)
+área = int(input("Número: "))
+print(cálculo(área))
---
+++ Exercícios - Introdução à programação com Python - Nilo Menezes/Exercício 8.04.py
@@ -0,0 +1,7 @@
+def cálculo (b, a):
+    return ((b*a)/2)
+
+n1 = int(input("Base: "))
+n2 = int(input("Altura: "))
+
+print(cálculo(n1, n2))
