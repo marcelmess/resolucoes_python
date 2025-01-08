046c811c002d1867e09231a6fba5e946f87cdca5
Marcel Cardoso <marcelmessiascardoso@gmail.com>
2025-01-08 20:11:35 -0300

    Add files via upload

---
+++ Exercícios - Introdução à programação com Python - Nilo Menezes/Exercício 8.13.py
@@ -0,0 +1,9 @@
+import random
+n=random.randint(1,10)
+for caractere in range(3):
+    x = int(input("Escolha um número entre 1 e 10:"))
+    if x==n:
+        print("Você acertou!")
+        break
+    else:
+        print("Você errou.")
