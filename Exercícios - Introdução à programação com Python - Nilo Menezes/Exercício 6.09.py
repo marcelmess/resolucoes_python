693e4e7e113461915adee93f59dfd5e8b3e19b12
Marcel Cardoso <marcelmessiascardoso@gmail.com>
2025-01-08 20:34:40 -0300

    Update and rename Exercício 6.9.py to Exercício 6.09.py

--- Exercícios - Introdução à programação com Python - Nilo Menezes/Exercício 6.09.py
+++ Exercícios - Introdução à programação com Python - Nilo Menezes/Exercício 6.09.py
@@ -3,7 +3,6 @@ L=[15,7,27,39]
 p=int(input("Digite o primeiro valor a procurar: "))
 p2=int(input("Digite o segundo valor a procurar: "))
 x=0
-
 while x<len(L):
     if L[x]==p:
         print("%d achado na posição %d" % (p,x))
